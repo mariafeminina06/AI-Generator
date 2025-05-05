122c20b585ae820158266cf43af6a148a20060b1
Maria Escalante <maria_feminina06@yahoo.com>
2025-05-05 08:54:34 +0200

    changed term eateries to restaurants

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generateTravel(event) {
   event.preventDefault();
   let destination = document.querySelector("#destination");
   let key = "72e8349d9ea254f743oacta0ef654b86";
-  let prompt = `User Instruction: Please provide list of popular tourist attractions, popular eateries, local delicacies, modes of transport, local currency and popular hotels in the area while visiting ${destination.value}.`;
+  let prompt = `User Instruction: Please provide list of popular tourist attractions, popular restaurants, local delicacies, modes of transport, local currency and popular hotels in the area while visiting ${destination.value}.`;
   let context =
     "You are an AI travel assistant. Please provide a travel guide based on the user instructions provided, providing the answer in basic HTML.  Do not include the ```htm`. Begin with the title `Travel Guide for `. Use a header list for each section of travel information and ol for the information. Be sure to follow the instructions and provide all needed category information.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
